6f48494bb6d24e8359102db36239b98fbefb0426
artmerlop <artmerlop@gmail.com>
2022-10-28 13:28:29 -0500

    Agrega push y drop al helper de storage

--- src/helpers.js
+++ src/helpers.js
@@ -1,33 +1,89 @@
-export function storage(action, key, data = null) {
-  const set = (key, data) => {
+export function storage(action, key, data = null, index = 0) {
+  const set = () => {
     if (data) {
-      return localStorage.setItem(key, JSON.stringify(data))
+      localStorage.setItem(key, JSON.stringify(data))
+      return data
     } else {
       return null
     }
   }
-  const get = (key) => {
+  const get = () => {
     try {
       return JSON.parse(localStorage.getItem(key))
     } catch {
       return null
     }
   }
-  const remove = (key) => {
+  const push = () => {
+    try {
+      let array = localStorage.getItem(key), result
+      if (!array) {
+        result = [data]
+      } else {
+        result = JSON.parse(array)
+        if (!Array.isArray(result)) {
+          throw new Error(`Key "${key}" is not an array so we can't push this item.`)
+        }
+        if (result.length > 0) {
+          const origin = [...result]
+          for (var i = 0; i < origin.length; i++) {
+            if (i === index) {
+              result[i] = data
+              result[i+1] = origin[i]
+            } else if (i > index) {
+              result[i+1] = origin[i]
+            }
+          }
+        } else {
+          result = [data]
+        }
+      }
+      localStorage.setItem(key, JSON.stringify(result))
+      return {error: null, data: result}
+    } catch (error) {
+      return {error: error.message, data: null}
+    }
+  }
+  const splice = () => {
+    try {
+      let array = localStorage.getItem(key), result
+      if (!array) {
+        throw new Error(`Key "${key}" is empty.`)
+      } else {
+        let idx = index
+        if (!idx && idx !== 0) {
+          idx = array.findIndex(item => item === data)
+        }
+        if (!array[idx]) {
+          throw new Error(`Index "${idx}" does not exist.`)
+        }
+        result = JSON.parse(array)
+        if (!Array.isArray(result)) {
+          throw new Error(`Key "${key}" is not an array so we can't push this item.`)
+        }
+        result.splice(idx, 1)
+      }
+      localStorage.setItem(key, JSON.stringify(result))
+      return {error: null, data: result}
+    } catch (error) {
+      return {error: error.message, data: null}
+    }
+  }
+  const remove = () => {
     return localStorage.removeItem(key)
   }
   switch (action) {
     case 'set':
-      return set(key, data)
-      break
+      return set()
     case 'get':
-      return get(key)
-      break
+      return get()
+    case 'push':
+      return push()
+    case 'splice':
+      return splice()
     case 'remove':
-      return remove(key)
-      break
+      return remove()
     default:
-      break
-      return
+      throw new Error('Invalid action, you can use "set", "get", "push", "splice" or "remove".')
   }
 }
